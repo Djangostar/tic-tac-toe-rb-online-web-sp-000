dc74ad558885332b51b34c4ea3bb114bad3f0b3c
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-04-09 00:39:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,6 +17,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(board)
-  user_input= input_to_index(board).to_i -1
+def input_to_index(user_input)
+  user_input.to_i -1
 end
